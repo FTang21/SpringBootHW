bbf98d98ae02e5434328fbb0070e6b0706128cbf
Franke Tang <franketang@gmail.com>
2024-12-16 10:52:35 -0600

    Fix mistakes mentioned in class

--- src/main/java/io/springboot/springboothw/controller/UniversityController.java
+++ src/main/java/io/springboot/springboothw/controller/UniversityController.java
@@ -3,6 +3,8 @@ package io.springboot.springboothw.controller;
 import io.springboot.springboothw.pojo.University;
 import io.springboot.springboothw.service.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +15,24 @@ import java.util.Map;
 @RestController
 public class UniversityController {
 
+    private final UniversityService universityService;
+
     @Autowired
-    private UniversityService universityService;
+    private UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    };
 
     @GetMapping("/search")
-    public Map<String, List<University>> getUniversitiesByCountries(@RequestParam(required = false) List<String> country) {
+    public ResponseEntity<Map<String, List<String>>> getUniversitiesByCountries(@RequestParam(required = false) List<String> country) {
         if (country == null || country.isEmpty()) {
-            return universityService.getAllUniversities();
+            return new ResponseEntity<>(
+                    universityService.getAllUniversities(),
+                    HttpStatus.OK
+            );
         }
-        return universityService.getUniversitiesByCountries(country);
+        return new ResponseEntity<>(
+                universityService.getUniversitiesByCountries(country),
+                HttpStatus.OK
+        );
     }
 }
--- src/main/java/io/springboot/springboothw/service/UniversityService.java
+++ src/main/java/io/springboot/springboothw/service/UniversityService.java
@@ -1,53 +1,13 @@
 package io.springboot.springboothw.service;
 
-import io.springboot.springboothw.exception.NotValidCountryException;
-import io.springboot.springboothw.pojo.University;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.List;
 
 @Service
-public class UniversityService {
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    private static final String API_URL = "http://universities.hipolabs.com/search";
-
-    public Map<String, List<University>> getAllUniversities() {
-        University[] universities = restTemplate.getForObject(API_URL, University[].class);
-        return Arrays.stream(universities)
-                .collect(Collectors.groupingBy(University::getCountry, Collectors.toList()));
-    }
-
-    public Map<String, List<University>> getUniversitiesByCountries(List<String> countries) {
-        List<CompletableFuture<Map<String, List<University>>>> futures = countries.stream()
-            .map(country -> CompletableFuture.supplyAsync(() -> getUniversitiesByCountry(country)))
-            .collect(Collectors.toList());
-        Map<String, List<University>> universitiesByCountries = new HashMap<>();
-        futures.forEach(future -> {
-            try {
-                universitiesByCountries.putAll(future.join());
-            } catch (Exception e) {
-                throw new NotValidCountryException("Error fetching data for one or more countries " + e.getMessage());
-            }
-        });
-        return universitiesByCountries;
-    }
-
-    private Map<String, List<University>> getUniversitiesByCountry(String country) {
-        String url = API_URL + "?country=" + country;
-        try {
-            University[] universities = restTemplate.getForObject(url, University[].class);
-            Map<String, List<University>> universitiesByCountries = new HashMap<>();
-            universitiesByCountries.put(country, Arrays.asList(universities));
-            return universitiesByCountries;
-        } catch (Exception e) {
-            throw new NotValidCountryException(country + " is not a valid country");
-        }
-    }
+public interface UniversityService {
+    Map<String, List<String>> getAllUniversities();
+    Map<String, List<String>> getUniversitiesByCountry(String universityName);
+    Map<String, List<String>> getUniversitiesByCountries(List<String> countries);
 }
